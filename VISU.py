331a9578524adc781019719a3c51493b1457c90e
Gabriel Araujo <gabs@cesar.school>
2024-05-19 23:30:09 -0300

    func VISU corrigida

--- VISU.py
+++ VISU.py
@@ -1,12 +1,7 @@
 def visualizar(cadastros):
-    try:
-        with open("BancoDados.txt", "r") as file:
-            pais = input("Digite o país a ser filtrado: ").capitalize()
-            for linha in file:
-                if pais in linha:
-                    print(linha)
-                else:
-                    print("O país não foi encontrado")
-    except FileNotFoundError:
-        print("Arquivo não encontrado")
+    file = open("BancoDados.txt", "r")
+    pais = input("Digite o país a ser filtrado: ").capitalize()
+    for linha in file:
+        if pais in linha:
+            print(linha)
     return cadastros
